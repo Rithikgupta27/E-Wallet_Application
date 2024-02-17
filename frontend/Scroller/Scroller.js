c29a175250a3d9a1bd6cd572e81fb7d595934880
rohilgupta <rohil2700@gmail.com>
2024-02-17 22:48:25 +0530

    scroller added

---
+++ frontend/Scroller/Scroller.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import  './Scroller.css'
+function Scroller() {
+  return (
+    <div>
+      <div class="scrolling-wrapper">
+          <div class="card"><h2>Card1</h2></div>
+          <div class="card"><h2>Card2</h2></div>
+          <div class="card"><h2>Card3</h2></div>
+          <div class="card"><h2>Card4</h2></div>
+          <div class="card"><h2>Card5</h2></div>
+          <div class="card"><h2>Card6</h2></div>
+          <div class="card"><h2>Card7</h2></div>
+          <div class="card"><h2>Card8</h2></div>
+          <div class="card"><h2>Card9</h2></div>
+     </div>
+    </div>
+  )
+}
+
+export default Scroller
