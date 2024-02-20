ef995c31af4ae7585260a585df05690c8ff17c0f
Rithikgupta27 <rithikg2700@gmail.com>
2024-02-20 22:42:54 +0530

    backend

--- backend/src/main/java/com/ePay/service/BillPaymentServiceImpl.java
+++ backend/src/main/java/com/ePay/service/BillPaymentServiceImpl.java
@@ -54,7 +54,7 @@ public class BillPaymentServiceImpl implements BillPaymentService {
 				transaction.setAmount(bill.getAmount());
 				transaction.setDescription(bill.getBillDescription());
 				transaction.setTransactionDate(LocalDateTime.now());
-				transaction.setTransactionType("Payment");
+				transaction.setTransactionType("Bill Payment");
 				transaction.setWallet(wallet);
 				tDao.save(transaction);
 				bill.setWallet(wallet);
